65ceb4df6871da124e29ec589b10d7e5a7389e7c
Ruben Schmidt Mällberg <ruben@spacemaker.ai>
2021-09-09 21:23:21 +0200

    password protect page

--- logic.js
+++ logic.js
@@ -1,7 +1,34 @@
-$('#menubar ul li a').on('click',function(event){
-    var $anchor = $(this);
-    $('html, body').animate({
-      scrollTop: $($anchor.attr('href')).offset().top + "px"
-    }, 1500);
-    event.preventDefault();
- });
+"use strict";
+
+function returnStatus(req, status, address) {
+  //console.log(req);
+  if (status == 200) {
+    window.location = address;
+  } else {
+    var input = document.getElementById("password");
+    input.value = "";
+    input.classList.add("is-invalid");
+    input.setAttribute("placeholder", "Nope! Prøv igjen!");
+  }
+}
+
+function fetchStatus(address) {
+  var client = new XMLHttpRequest();
+  client.onreadystatechange = function () {
+    // in case of network errors this might not give reliable results
+    if (this.readyState == 4) returnStatus(this, this.status, address);
+  };
+  client.open("HEAD", address);
+  client.send();
+}
+
+function loadPage(pwd) {
+  var hash = CryptoJS.SHA1(pwd);
+  var url = hash + "/index.html";
+  fetchStatus(url);
+}
+
+const button = document.getElementById("password-submit");
+button.addEventListener("click", (event) => {
+  loadPage(document.getElementById("password").value);
+});
